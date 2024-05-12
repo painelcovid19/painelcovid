57dbfa45bdae0ecd41e77e494c2347ff887cc123
josedembo <josedembo18@aluno.unilab.edu.br>
2024-05-12 16:59:54 -0300

    feat: Add oracle env variables to config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os 
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
@@ -11,6 +15,9 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    ORACLE_DB_PASSWORD=os.environ.get("ORACLE_DB_USER_PASSWORD")
+    ORACLE_DB_USERNAME=os.environ.get("ORACLE_DB_USERNAME")
+    ORACLE_DB_DSN=os.environ.get("ORACLE_DB_DSN")
     
 class TestingConfig(Config):
     TESTING = True
