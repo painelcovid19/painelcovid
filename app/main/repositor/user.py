9ccd373a71ad273c9b53b259a5cc011fbdea7548
josedembo <josedembo18@aluno.unilab.edu.br>
2024-05-12 06:17:50 -0300

    feat: Update user repositor
    - Add the username parameter to the methods
    - Add a new custumised select methods to retrive user data

--- app/main/repositor/user.py
+++ app/main/repositor/user.py
@@ -1,43 +1,87 @@
-from app.main.supabase import supabase
-from datetime import datetime
-import json
+from app.database.connection import DBConnectionHandler
+from app.main.models.user import User
+from sqlalchemy.orm.exc import NoResultFound
 
-class UserRepositor():
-    
-    def __init__(self, table_name) -> None:
-        self.table_name = table_name
-        pass
-    def insert(self, name:str, email:str, password:str):
-        user = {
-            "name": name,
-            "email": email,
-            "password": password
-        }
-        response = supabase.table(self.table_name).insert(user).execute()
-        return response.data
 
+class UserRepositor:
     
-    def select_all(self) -> list[dict]:
-        response  = supabase.table(self.table_name).select("*").execute()
-        return response.data
-    
-    def select_by_id(self, id) -> list[dict]:
-        response = supabase.table(self.table_name).select("*").eq("id", f"{id}").execute()
-        return response.data
-    
-    def select_by_email(self, email):
-        response = supabase.table(self.table_name).select("*").eq("email", f"{email}").execute()
-        return response.data
-    
-    def delete(self, id):
-        response = supabase.table(self.table_name).delete().eq("id", id).execute()
-        return response
+    def create(self, email, password, full_name, username):
+        with DBConnectionHandler() as db:
+            try:
+                user = User(email=email, password=password, 
+                             full_name=full_name, username=username)
+                db.session.add(user)
+                db.session.commit()
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
+            
+    def select(self):
+        with DBConnectionHandler() as db:
+            try:
+                data = db.session.query(User).all()
+                return data
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+
+    def select_by_id(self, id:int):
+        with DBConnectionHandler() as db:
+            try:
+                user = db.session.query(User).filter(User.id==id).first()
+                return user
+            except NoResultFound:
+                return None
+            except Exception as exception:
+                raise exception
         
-    
-    def update(self, id, data:dict):
-        # data["updated_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
-        # data = json.dumps(data)
-        response = supabase.table(self.table_name).update(data).eq("id",id).execute()
-        return response
-    
+    def select_by_email(self, email):
+        with DBConnectionHandler() as db:
+            try:
+                user = db.session.query(User).filter(User.email==email).first()
+                return user
+            except NoResultFound:
+                return None
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+        
+    def select_by_username(self, username):
+        with DBConnectionHandler() as db:
+            try:
+                user = db.session.query(User).filter(User.username==username).first()
+                return user
+            except NoResultFound:
+                return None
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+                
+    def update(self, id:int, full_name):
+        with DBConnectionHandler() as db:
+            try:
+                db.session.query(User).filter(User.id==id).update({
+                    "full_name":full_name 
+                })
+                db.session.commit()
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+            
+    def delete(self, id:int):
+        with DBConnectionHandler() as db:
+            try:
+                db.session.query(User).filter(User.id==id).delete()
+                db.session.commit()
+            except Exception as ex:
+                db.session.rollback()
+                raise ex
+    def delete_by_username(self, username:str):
+        with DBConnectionHandler() as db:
+            try:
+                db.session.query(User).filter(User.username==username).delete()
+                db.session.commit()
+            except Exception as ex:
+                db.session.rollback()
+                raise ex    
     
