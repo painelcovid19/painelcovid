11919b7d6de77a88c20678ca8917a9dbd9602d94
josedembo <josedembo18@aluno.unilab.edu.br>
2024-07-24 07:53:23 -0300

    feat: update user repositor

--- app/main/repositor/user.py
+++ app/main/repositor/user.py
@@ -1,56 +1,40 @@
 from app.database.connection import DBConnectionHandler
 from app.main.models.user import User
 from sqlalchemy.orm.exc import NoResultFound
-
+from app.main._supabase import supabase
 
 class UserRepositor:
     
+    def __init__(self, table_name) -> None:
+        self.__TABLE_NAME= table_name
+    
     def create(self, email, password, full_name, username):
-        with DBConnectionHandler() as db:
-            try:
-                user = User(email=email, password=password, 
-                             full_name=full_name, username=username)
-                db.session.add(user)
-                db.session.commit()
-            except Exception as exception:
-                db.session.rollback()
-                raise exception
+       supabase.table(self.__TABLE_NAME).insert({
+           "full_name": full_name,
+           "email": email,
+           "username": username,
+           "password": password
+       }).execute()
             
     def select(self):
-        with DBConnectionHandler() as db:
-            try:
-                data = db.session.query(User).all()
-                return data
-            except Exception as ex:
-                db.session.rollback()
-                raise ex
+        response = supabase.table(self.__TABLE_NAME).select("*").execute()
+        return response.data
 
     def select_by_id(self, id:int):
-        with DBConnectionHandler() as db:
-            try:
-                user = db.session.query(User).filter(User.id==id).first()
-                return user
-            except NoResultFound:
-                return None
-            except Exception as exception:
-                raise exception
+        user_response = supabase.table(self.__TABLE_NAME).select("*").eq("id", id).execute()
+        return user_response.data
         
     def select_by_email(self, email):
-        with DBConnectionHandler() as db:
-            try:
-                user = db.session.query(User).filter(User.email==email).first()
-                return user
-            except NoResultFound:
-                return None
-            except Exception as ex:
-                db.session.rollback()
-                raise ex
+        user_response = supabase.table(self.__TABLE_NAME).select("*").eq("email", email).execute()
+        return user_response.data
         
     def select_by_username(self, username):
         with DBConnectionHandler() as db:
             try:
-                user = db.session.query(User).filter(User.username==username).first()
-                return user
+                user_response = supabase.table(self.__TABLE_NAME).select("*").eq("username", username).execute()
+                print(user_response.data)
+                print(type(user_response.data))
+                return user_response.data
             except NoResultFound:
                 return None
             except Exception as ex:
@@ -60,8 +44,8 @@ class UserRepositor:
     def update(self, id:int, data:dict):
         with DBConnectionHandler() as db:
             try:
-                db.session.query(User).filter(User.id==id).update(data)
-                db.session.commit()
+                user_response = supabase.table(self.__TABLE_NAME).update(data).eq("id", id).execute()
+                return user_response.data
             except Exception as ex:
                 db.session.rollback()
                 raise ex
@@ -69,16 +53,16 @@ class UserRepositor:
     def delete(self, id:int):
         with DBConnectionHandler() as db:
             try:
-                db.session.query(User).filter(User.id==id).delete()
-                db.session.commit()
+                user_response = supabase.table(self.__TABLE_NAME).delete().eq("id", id).execute()
+                return user_response.data
             except Exception as ex:
                 db.session.rollback()
                 raise ex
     def delete_by_username(self, username:str):
         with DBConnectionHandler() as db:
             try:
-                db.session.query(User).filter(User.username==username).delete()
-                db.session.commit()
+                user_response = supabase.table(self.__TABLE_NAME).delete().eq("username", username).execute()
+                return user_response
             except Exception as ex:
                 db.session.rollback()
                 raise ex    
