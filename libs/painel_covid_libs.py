6ca4a5f2fa3212e38a04eefe9dc98e354584cff3
josedembo <josedembo18@aluno.unilab.edu.br>
2024-07-24 08:12:15 -0300

    feat: update libs functions

--- libs/painel_covid_libs.py
+++ libs/painel_covid_libs.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv, find_dotenv
 from storage.minio.minio_pc import minio
+from storage.oci.oracle_cloud_storage import PainelCovidStorageOCI
 import pandas as pd
 import os
 
@@ -13,7 +14,8 @@ def save_data(data:pd.DataFrame, file_name:str, source_type:str, bucket_name:str
     if source_type == "minio":
         minio.put_object(data, file_name, source_type, default_bucket_name)
     elif source_type == "oci":
-        pass
+        s3 = PainelCovidStorageOCI()
+        s3.put_object(data, file_name, source_type, default_bucket_name)
     else:
         pass
     
@@ -22,6 +24,8 @@ def read_data(file_name:str, source_type:str) -> pd.DataFrame:
     if source_type == "minio":
         minio.read_object(file_name, source_type)
     elif source_type == "oci":
-        pass
+        oci = PainelCovidStorageOCI()
+        dataframe = oci.read_object(file_name, source_type, default_bucket_name)
+        return dataframe
     else:
         pass
